71ea423da8ed54174927f276bde1dbf85456ded0
solimanzein900-lang <solimanzein900@gmail.com>
2026-01-19 16:17:44 +0100

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,10 @@
+import messageCreate from '../events/messageCreate.js';
+import interactionCreate from '../events/interactionCreate.js';
 
+export function registerEvents(client) {
+    client.on('messageCreate', messageCreate);
+    client.on('interactionCreate', interactionCreate);
+}
+
+// Add helpers for creating dropdowns, embeds, etc.
+// Add your handlePing and handleInteraction logic here
