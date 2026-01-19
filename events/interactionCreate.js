eb6ccd6b478e29d1d4eb394ec8d68fe04cc66252
solimanzein900-lang <solimanzein900@gmail.com>
2026-01-19 16:17:15 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,6 @@
+import { handleInteraction } from '../utils/helpers.js';
 
+export default async function interactionCreate(interaction) {
+    if (!interaction.isButton() && !interaction.isSelectMenu()) return;
+    handleInteraction(interaction);
+}
