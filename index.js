459c624f8f4b51180f9d5b703dddda22fe9019c5
solimanzein900-lang <solimanzein900@gmail.com>
2026-01-19 15:39:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 import { Client, GatewayIntentBits, Events } from 'discord.js';
-import { handlePing } from './events/pingHandler.js';
-import { handleInteraction } from './events/interactionHandler.js';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import { registerEvents } from './utils/helpers.js';
 
-const client = new Client({ 
+dotenv.config();
+
+const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
-    ] 
+    ]
 });
 
-client.once(Events.ClientReady, () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
-
-// Ping-based price list responses
-client.on(Events.MessageCreate, handlePing);
-
-// Button / dropdown interactions
-client.on(Events.InteractionCreate, handleInteraction);
+// Register events
+registerEvents(client);
 
 client.login(process.env.TOKEN);
