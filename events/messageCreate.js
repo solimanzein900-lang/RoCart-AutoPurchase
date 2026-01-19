a76172a8a9b1843ce727f50dbd44da7c32c5adc1
solimanzein900-lang <solimanzein900@gmail.com>
2026-01-19 16:16:32 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,13 @@
+import { roles } from '../config/roles.js';
+import { handlePing } from '../utils/helpers.js';
 
+export default async function messageCreate(message) {
+    if (message.author.bot) return;
+
+    // Check if the message contains a role ping
+    Object.entries(roles).forEach(([key, roleId]) => {
+        if (message.mentions.roles.has(roleId)) {
+            handlePing(message, key);
+        }
+    });
+}
