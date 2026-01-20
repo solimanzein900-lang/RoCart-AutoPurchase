7cc1ed16d8aad23cd4fdd94877256366ca40e77a
solimanzein900-lang <solimanzein900@gmail.com>
2026-01-20 18:16:22 +0100

    Update paymentHandlers.js

--- utils/paymentHandlers.js
+++ utils/paymentHandlers.js
@@ -1,14 +1,48 @@
+import { EmbedBuilder } from "discord.js";
+
 export function getPaymentEmbed(method, total) {
     switch(method) {
         case 'PayPal':
-            return `Your total is ${total} USD.\nPlease send it to \`solimanzein860@gmail.com\` through FnF (friends and family).\nAfter paying, send a screenshot of the transaction in the ticket.`;
+            return new EmbedBuilder()
+                .setTitle("**Payment Instructions**")
+                .setDescription(
+                    `__PayPal Payment Instructions__\n\n` +
+                    `<:reply_continued:1463044510392254631> Your total is **$${total} USD**\n\n` +
+                    `<:reply_continued:1463044510392254631> Please send **$${total}** to **solimanzein860@gmail.com**\n\n` +
+                    `<:reply_continued:1463044510392254631> After paying, please send a screenshot in this ticket`
+                )
+                .setColor(0x2b2d31);
+
         case 'Litecoin':
-            return `Your total is ${total} USD.\nPlease send it to this Litecoin address \`LRhUVpYPbANmtczdDuZbHHkrunyWJwEFKm\`.\nAfter paying, send a screenshot of the transaction in the ticket.`;
+            return new EmbedBuilder()
+                .setTitle("**Payment Instructions**")
+                .setDescription(
+                    `__Litecoin Payment Instructions__\n\n` +
+                    `<:reply_continued:1463044510392254631> Your total is **$${total} USD**\n\n` +
+                    `<:reply_continued:1463044510392254631> Please send exactly **$${total}** to this LTC address  \n` +
+                    `<:reply_continued:1463044510392254631> __(click to copy)__\n\n` +
+                    `<:reply_continued:1463044510392254631> \`LRhUVpYPbANmtczdDuZbHHkrunyWJwEFKm\`\n\n` +
+                    `<:reply_continued:1463044510392254631> After paying, please send a screenshot in this ticket`
+                )
+                .setColor(0x2b2d31);
+
         case 'Card':
         case 'Google Pay':
         case 'Apple Pay':
-            return `Your total is ${total} USD.\nClick the Purchase button below, enter exactly ${total}, and complete payment.\nAfter paying, send a screenshot of the transaction.`;
+            return new EmbedBuilder()
+                .setTitle("**Payment Instructions**")
+                .setDescription(
+                    `__${method} Payment Instructions__\n\n` +
+                    `<:reply_continued:1463044510392254631> Your total is **$${total} USD**\n\n` +
+                    `<:reply_continued:1463044510392254631> Click the **Purchase** button below to complete your purchase\n\n` +
+                    `<:reply_continued:1463044510392254631> After paying, please send a screenshot in this ticket`
+                )
+                .setColor(0x2b2d31);
+
         default:
-            return `Payment method not supported.`;
+            return new EmbedBuilder()
+                .setTitle("**Payment Instructions**")
+                .setDescription("Payment method not supported.")
+                .setColor(0xff0000);
     }
 }
